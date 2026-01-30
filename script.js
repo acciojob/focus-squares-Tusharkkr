ad943ee8eb9f119d26e5d8ef3fc29e179c92d16f
Tusharkkr <narutoboi41@gmail.com>
2026-01-31 00:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-//your JS code here. If required.
-let sq1 = document.querySelector('#square1')
-        let sq2 = document.querySelector('#square2')
-        let sq3 = document.querySelector('#square3')
 
-        sq1.addEventListener("mouseenter", () => {
-            sq2.style.backgroundColor='#6F4E37'
-            sq3.style.backgroundColor='#6F4E37'
-        });
-        sq1.addEventListener('mouseleave',()=>{
-            sq2.style.backgroundColor='#E6E6FA'
-            sq3.style.backgroundColor='#E6E6FA'
-        });
+    let squares = document.querySelectorAll('.square');
 
+    for (let i = 0; i < squares.length; i++) {
 
-        sq2.addEventListener("mouseenter", () => {
-            sq1.style.backgroundColor='#6F4E37'
-            sq3.style.backgroundColor='#6F4E37'
-        });
-        sq2.addEventListener('mouseleave',()=>{
-            sq1.style.backgroundColor='#E6E6FA'
-            sq3.style.backgroundColor='#E6E6FA'
+        squares[i].addEventListener("mouseenter", () => {
+            for (let j = 0; j < squares.length; j++) {
+                if (i !== j) {
+                    squares[j].style.backgroundColor = '#6F4E37';
+                }
+            }
         });
 
+        squares[i].addEventListener("mouseleave", () => {
+            for (let j = 0; j < squares.length; j++) {
+                squares[j].style.backgroundColor = '#E6E6FA';
+            }
+        });
+    }
 
-        sq3.addEventListener("mouseenter", () => {
-            sq2.style.backgroundColor='#6F4E37'
-            sq1.style.backgroundColor='#6F4E37'
-        });
-        sq3.addEventListener('mouseleave',()=>{
-            sq2.style.backgroundColor='#E6E6FA'
-            sq1.style.backgroundColor='#E6E6FA'
-        });
